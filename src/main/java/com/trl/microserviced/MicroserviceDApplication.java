8184ad8617895a99c23846c618667b90ace63947
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-12 17:44:34 +0200

    Configuration Eureka Client.

--- src/main/java/com/trl/microserviced/MicroserviceDApplication.java
+++ src/main/java/com/trl/microserviced/MicroserviceDApplication.java
@@ -2,7 +2,9 @@ package com.trl.microserviced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceDApplication {
 
